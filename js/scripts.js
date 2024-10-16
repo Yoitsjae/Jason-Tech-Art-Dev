03107b766824249375854df507341551f38a1d54
Yoitsjae <ncubejason896@gmail.com>
2024-10-16 22:37:57 +0200

    first_commit

---
+++ js/scripts.js
@@ -0,0 +1,5 @@
+// Add any JavaScript functionality here
+document.getElementById("contact-form").onsubmit = function(event) {
+  event.preventDefault();
+  alert("Your message has been sent!"); // Placeholder action for form submission
+};
